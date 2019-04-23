2c90fabe1a4df0f147889f7811f1fa7fe2337a85
Deepak <deepak.sharma@bainslabs.com>
2019-04-23 18:21:01 +0530

    New App Added, Readme Added, Environment File Added

---
+++ README.md
@@ -0,0 +1,6 @@
+### Web Template
+
+#### Installation Steps:
+1. Run `sudo pip3 install pipenv`
+2. Run `pipenv install` to install all dependencies.
+3. Create `.env` from `.env_default`
--- webtemp/settings.py
+++ webtemp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^^0#1za(vb23br4*(e^=2dxf)pnf2&ylo+34&i(m(%e-0!8*7z'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
 ]
 
 MIDDLEWARE = [

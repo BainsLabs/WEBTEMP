9fbbbb54bfc5f73463cf761118b57866dde490ea
Deepak <deepak.sharma@bainslabs.com>
2019-04-25 17:04:12 +0530

    CMS Models Added

--- webtemp/settings.py
+++ webtemp/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'core',
+    'content_manager',
 ]
 
 MIDDLEWARE = [
